8521d314630f049032bed03e78afc851884cd24a
cordovabarbara <aliendocordova@gmail.com>
2025-09-18 16:52:28 -0500

    "Add end-to-end test for login, shopping, and checkout flow"

--- tests/test_e2e_purchase.py
+++ tests/test_e2e_purchase.py
@@ -1,5 +1,6 @@
-import pytest
 from pages.login import LoginPage
+from pages.shopping import ShopPage
+from pages.checkout import CheckoutPage
 
 def test_login(browserInstance):
     driver = browserInstance
@@ -15,3 +16,19 @@ def test_login(browserInstance):
 
     # Verification
     assert "inventory" in driver.current_url
+
+    #Shop actions
+    shop_page = ShopPage(driver)
+    shop_page.add_all_products_to_cart()
+    shop_page.go_to_cart()
+
+    #Checkout
+    checkout_page =CheckoutPage(driver)
+    checkout_page.checkout()
+    checkout_page.enter_information("Barbara", "Cordova", "12345")
+    checkout_page.continue_checkout()
+    checkout_page.finish_checkout()
+
+    success_text = checkout_page.success_message()
+    assert "thank you for your order" in success_text.lower()
+
